0caabc9db2645c517c89c232c8d117a5d86b57b2
Seshu <scharla@purdue.edu>
2023-08-01 12:08:19 -0700

    commiting for remote

--- static/read_data.py
+++ static/read_data.py
@@ -1,13 +1,5 @@
 import numpy as np
 
-# Loading data -----------------------------------------------------------------
-V_in = 16.15
-dir = "../../StaticData/WithProp_PWM_in/p_steps_data"
-omega_50 = np.loadtxt(dir+"/omega.csv")
-F_50 = np.loadtxt(dir+"/Fx.csv")
-M_50 = np.loadtxt(dir+"/Mx.csv")
-omega_10 = np.loadtxt(dir+"_fine/omega_static.csv")
-F_10 = np.loadtxt(dir+"_fine/Fx.csv")
-M_10 = np.loadtxt(dir+"_fine/Mx.csv")
-
-
+# Reading with prop fine data
+dir = "../../StaticData/WithProp_PWM_in/p_steps_data_fine/"
+esc_data = np.loadtxt(dir+"ESC_data_fine.csv")
