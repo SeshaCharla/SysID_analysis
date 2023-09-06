827829e730587b83280d5fbcb9cd1d8abf08acfd
Seshu <scharla@purdue.edu>
2023-09-06 11:20:28 -0400

    g_w function definitions

---
+++ static/norm_omega.py
@@ -0,0 +1,11 @@
+
+a = 22.6
+b = -51
+
+def gw(u_p):
+    """The non-linear input transformation function"""
+    return a*np.log(u_p - 1110) + b
+
+def dgw(u_p):
+    """ Derivative"""
+    return a/(u_p - 1110)
